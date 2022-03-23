a8ee38bef435ae4d6794c0f4b7910975198f6ad4
Deni434 <66267728+Deni434@users.noreply.github.com>
2022-03-23 14:13:19 -0700

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,26 @@
+package entity;
+
+public class Department {
+		private int departmentid;
+		private String name;
+		
+		
+		public int getDepartmentid() {
+			return departmentid;
+		}
+		
+		public void setDepartmentid(int departmentid) {
+			this.departmentid = departmentid;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+		
+}
---
+++ Employee.java
@@ -0,0 +1,44 @@
+package entity;
+
+public class Employee {
+
+	private int id;
+	private String esalary;
+	private String ename;
+	private String edepartment;
+
+	public String getEsalary() {
+		return esalary;
+	}
+
+	public void setEsalary(String esalary) {
+		this.esalary = esalary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEdepartment() {
+		return edepartment;
+	}
+
+	public void setEdepartment(String edepartment) {
+		this.edepartment = edepartment;
+	}
+   
+   
+
+}
